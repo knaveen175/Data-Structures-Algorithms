1a52a842047cabe35e3841504851ddf44e8ac300
Naveen Kushwaha <naveen17593@gmail.com>
2026-08-07 21:30:56 +0530

    A08 04,05,06

---
+++ 08_05.cpp
@@ -0,0 +1,46 @@
+/*
+Assignment : 08
+Topic : Stack using Arrays
+05  ||       In Question-1, define a method to pop the top element of the Stack.
+
+*/
+#include<iostream>
+using namespace std;
+
+class Stack
+{
+    private:
+        int Capacity, top, *ptr;
+    public:
+        Stack()
+        {
+            Capacity = 0, top = -1, ptr = nullptr;
+        }
+        Stack(int c)
+        {
+            Capacity = c, top = -1;
+            ptr = new int[Capacity];
+        }
+        void push(int element)
+        {
+            if(top >= Capacity-1)
+                return;
+            top++;
+            ptr[top] = element;
+        }
+        int peek()
+        {
+            if(top == -1)
+            {
+                cout<<"Stack is Empty\n";
+                return -1;
+            }
+            return ptr[top];
+        }
+        void pop()
+        {
+            if(top == -1)
+                return;
+            top--;
+        }
+};
