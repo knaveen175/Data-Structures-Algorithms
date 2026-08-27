4646b491f6da2db115ee5c2ffdf2bce7396e08b9
Naveen Kushwaha <naveen17593@gmail.com>
2026-08-27 16:18:05 +0530

    A12 05,06

---
+++ 12_05.cpp
@@ -0,0 +1,73 @@
+/*
+Assignment : 12
+Topic : Deque
+05  ||       In Question 1, define a method to delete front element.
+
+*/
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int item;
+    node *prev;
+    node *next;
+};
+
+class Deque
+{
+    private:
+        node *front;
+        node *rear;
+    public:
+        Deque()
+        {
+            front = nullptr;
+            rear = nullptr;
+        }
+        void Insert_at_Front(int data)
+        {
+            node *t = new node;
+            t->item = data;
+            t->prev = nullptr;
+            if (front == nullptr)
+            {
+                t->next = nullptr;
+                front = t;
+                rear = t;
+                return;
+            }
+            t->next = front;
+            front->prev = t;
+            front = t;
+        }
+        void Insert_at_Rear(int data)
+        {
+            node *t = new node;
+            t->item = data;
+            t->next = nullptr;
+            if (rear == nullptr)
+            {
+                t->prev = nullptr;
+                rear = t;
+                front = t;
+                return;
+            }
+            t->prev = rear;
+            rear->next = t;
+            rear = t;
+        }
+        void Delete_Front()
+        {
+            if (front == nullptr)
+                throw("Deque is Empty");
+                
+            node *t = front;
+            front = front->next;
+            delete t;
+            if (front == nullptr)
+                rear = nullptr;
+            else
+                front->prev = nullptr;
+        }
+};
